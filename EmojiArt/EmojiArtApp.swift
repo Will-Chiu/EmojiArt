60c67eb024a845a20e94bed73541cf344aa1c9b7
Will Chiu <chiukayu@gmail.com>
2022-03-07 19:00:30 +0000

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Entangled Mind on 7/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
